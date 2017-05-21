41f7446e15989bd92d96afb80e11d1fd17c2ddc0
Alexandre Siqueira <alex@Mac-de-Alexandre.local>
2017-05-20 23:18:44 -0300

    First version

---
+++ iOS/magsample/magsample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magsample
+//
+//  Created by Alexandre Siqueira on 20/05/17.
+//  Copyright © 2017 Alexandre Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
